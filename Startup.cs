893f86eea5a4c35933d673be132a1139951fbbb2
TumTummai <tumtummai@MacBook-Air-khxng-TumTummai.local>
2021-06-30 21:58:21 +0700

    Test1

--- Startup.cs
+++ Startup.cs
@@ -46,7 +46,7 @@ namespace Dtest
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dtest v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
